ece270cce14bd3c6a563104a053d49da6153c7b1
YabTek <yabtekuam@gmail.comremote: Invalid username or password>
2024-04-11 06:17:29 +0300

    add chapter routes

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -2,10 +2,13 @@
 import departmentRouter from "./department";
 import userRouter from "./user";
 import courseRouter from "./course"
+import chapterRouter from "./chapter"
 
 
 export default {
   userRouter,
   departmentRouter,
   courseRouter,
+  chapterRouter,
+
 };
